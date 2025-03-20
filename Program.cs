4ba8a86f1b732a8e76ad070ae79690b58fb731f0
pavel <lutskovp@gmail.com>
2025-03-19 23:38:34 -0700

    adds graphql server to app at startup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGraphQLServer();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
 
 app.Run();
