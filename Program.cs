b6a166d250cf1c1a50593cc05e1b2abd9e793555
pavel <lutskovp@gmail.com>
2025-03-26 22:29:19 -0700

    adds cors and default apollo redirect

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
 using Odyssey.MusicMatcher;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddGraphQLServer()
     .AddQueryType<Query>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy( builder => {
+        builder
+               .WithOrigins("https://studio.apollographql.com")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+
+app.MapGet("/", redirect => {
+    redirect.Response.Redirect("/graphql");
+    return Task.CompletedTask;
+});
 app.MapGraphQL();
 
 app.Run();
