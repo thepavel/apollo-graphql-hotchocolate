78562d405f4774452c05e938e3bfbe9e87c88111
pavel <lutskovp@gmail.com>
2025-03-26 23:08:55 -0700

    adds Playlist documentation

--- Types/Playlist.cs
+++ Types/Playlist.cs
@@ -1,10 +1,16 @@
 namespace Odyssey.MusicMatcher;
 
+[GraphQLDescription("A curated collection of tracks designed for a specific activity or emotion")]
 public class Playlist
 {
+    [GraphQLDescription("Unique identifier for the playlist")]
     [ID]
     public string Id { get; }
+
+    [GraphQLDescription("The name of the playlist")]
     public string Name { get; set; }
+    
+    [GraphQLDescription("Describes what the listener should expect")]
     public string? Description { get; set; }
 
     public Playlist(string id, string name)
