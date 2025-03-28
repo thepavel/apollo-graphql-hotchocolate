22dcbbca3af72f7138835b4cd2dc8a4e0a5191ca
pavel <lutskovp@gmail.com>
2025-03-27 22:56:40 -0700

    refactor: primary constructor

--- Types/Playlist.cs
+++ Types/Playlist.cs
@@ -1,21 +1,15 @@
 namespace Odyssey.MusicMatcher;
 
 [GraphQLDescription("A curated collection of tracks designed for a specific activity or emotion")]
-public class Playlist
+public class Playlist(string id, string name)
 {
     [GraphQLDescription("Unique identifier for the playlist")]
     [ID]
-    public string Id { get; }
+    public string Id { get; } = id;
 
     [GraphQLDescription("The name of the playlist")]
-    public string Name { get; set; }
-    
+    public string Name { get; set; } = name;
+
     [GraphQLDescription("Describes what the listener should expect")]
     public string? Description { get; set; }
-
-    public Playlist(string id, string name)
-    {
-        Id = id;
-        Name = name;
-    }
 }
