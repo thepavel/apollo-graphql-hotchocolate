95c091ce08448531671d512fc9af71cc13bb8f27
pavel <lutskovp@gmail.com>
2025-04-12 23:22:03 -0700

    replaces featuredplaylist response with spotifyservice

--- Types/Playlist.cs
+++ Types/Playlist.cs
@@ -1,14 +1,28 @@
+using SpotifyWeb;
+
 namespace Odyssey.MusicMatcher;
 
 [GraphQLDescription("A curated collection of tracks designed for a specific activity or emotion")]
-public class Playlist(string id, string name)
+public class Playlist
 {
+    public Playlist(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public Playlist(PlaylistSimplified obj)
+    {
+        Id = obj.Id;
+        Name = obj.Name;
+        Description = obj.Description;
+    }
+
     [GraphQLDescription("Unique identifier for the playlist")]
     [ID]
-    public string Id { get; } = id;
+    public string Id { get; }
 
     [GraphQLDescription("The name of the playlist")]
-    public string Name { get; set; } = name;
+    public string Name { get; set; }
 
     [GraphQLDescription("Describes what the listener should expect")]
     public string? Description { get; set; }
--- Types/Query.cs
+++ Types/Query.cs
@@ -1,17 +1,19 @@
+using System.Threading.Tasks;
+using SpotifyWeb;
+
 namespace Odyssey.MusicMatcher;
 
 [GraphQLDescription("An API that delivers music information including artists, albums, and playlists")]
 public class Query
 {
     [GraphQLDescription("Playlists hand-picked to be featured to all users.")]
-    public List<Playlist> FeaturedPlaylists()
+    public async Task<List<Playlist>> FeaturedPlaylists(SpotifyService spotifyService)
     {
-        return
-       [
-           new Playlist("1", "GraphQL Groovin'"),
-           new Playlist("2", "Graph Explorer Jams"),
-           new Playlist("3", "Interpretive GraphQL Dance")
-       ];
+        var response = await spotifyService.GetFeaturedPlaylistsAsync();
+        var items = response.Playlists.Items;
+        var playlists = items.Select(p => new Playlist(p)).ToList();
+        return [.. playlists];
+        
     }
 }
 
