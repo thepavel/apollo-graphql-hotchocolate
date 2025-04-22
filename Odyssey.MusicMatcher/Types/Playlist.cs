549578cdf7e473fd80c12adf28c3097d6fd44e24
pavel <lutskovp@gmail.com>
2025-04-21 21:54:26 -0700

    removes tracks from playlist for demo

--- Odyssey.MusicMatcher/Types/Playlist.cs
+++ Odyssey.MusicMatcher/Types/Playlist.cs
@@ -5,8 +5,6 @@ namespace Odyssey.MusicMatcher;
 [GraphQLDescription("A curated collection of tracks designed for a specific activity or emotion")]
 public class Playlist
 {
-    private List<Track>? _tracks;
-
 
     public Playlist(string id, string name)
     {
@@ -19,8 +17,6 @@ public class Playlist
         Id = obj.Id;
         Name = obj.Name;
         Description = obj.Description;
-
-        _tracks = [.. obj.Tracks.Items.Select(item => new Track(item.Track))];
 
     }
 
@@ -41,15 +37,4 @@ public class Playlist
     [GraphQLDescription("Describes what the listener should expect")]
     public string? Description { get; set; }
 
-    [GraphQLDescription("The playlist's tracks.")]
-    public async Task<List<Track>> Tracks(SpotifyService spotifyService)
-    {
-        if (_tracks == null)
-        {
-            var response = await spotifyService.GetPlaylistsTracksAsync(this.Id);
-            _tracks = [.. response.Items.Select(item => new Track(item.Track))];
-        }
-        return _tracks;
-
-    }
 }
